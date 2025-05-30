dd00232d310a4b02aff346985c819b624ca337f3
vvu060 <vvu060@gmail.com>
2025-05-30 12:16:46 +0530

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
       <h1 className='text-5xl text-red-400 font-bold underline'>Hello World</h1>
+      <Button>Click me</Button>
     </>
   );
 }
